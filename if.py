6aa469e30525226deb904da0a9db356c86eaa046
Sanjaykumar-95 <84496961+Sanjaykumar-95@users.noreply.github.com>
2021-05-21 10:42:49 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,12 @@
+a=10
+if a<20:
+   print('a is less than 20')
+   print('hi,Sanjay')
+
+#expected output
+   #a is less than 20
+   #hi,Sanjay
+
+#original output
+   #a is less than 20
+   #hi,Sanjay
